1f76a6b040b6b91565dbe8b962d97a776be93fc2
timothyveldhuizen <timothy.veldhuizen@outlook.com>
2020-08-12 08:42:29 +0200

    implemented record table

--- react/app/src/components/FileUpload.js
+++ react/app/src/components/FileUpload.js
@@ -4,20 +4,26 @@ import RecordTable from './RecordTable';
 
 function FileUpload() {
     const [validatedRecords, setValidatedRecords] = useState([]);
+    const [fileIsUploaded, setFileIsUploaded] = useState(false);
 
     function handleFileUploadChange(e) {
         const file = e.target.files[0];
-        console.log(e.target.files[0])
-        // file.stream().getReader().read().then(readerObject => console.log('readerObject',readerObject));
-        // file.arrayBuffer().then(buffer => console.log('buffer',buffer));
-        file.text().then(text => {
-            console.log('text', text)
-            const parsedFile = parseFile(text, file.type)
-            console.log('parsedFile', parsedFile)
-            setValidatedRecords(validateRecords(parsedFile))
-            console.log('validatedRecords', validatedRecords)
-
-        });
+        if(file) {
+            console.log(e.target.files[0])
+            // file.stream().getReader().read().then(readerObject => console.log('readerObject',readerObject));
+            // file.arrayBuffer().then(buffer => console.log('buffer',buffer));
+            file.text().then(text => {
+                console.log('text', text)
+                const parsedFile = parseFile(text, file.type)
+                console.log('parsedFile', parsedFile)
+                setValidatedRecords(validateRecords(parsedFile))
+                console.log('validatedRecords', validatedRecords)
+                setFileIsUploaded(true);
+    
+            });
+        }
+        setValidatedRecords([]);
+        setFileIsUploaded(false);
     }
 
     return (
@@ -29,7 +35,10 @@ function FileUpload() {
                 accept="text/csv, text/xml"
                 onChange={handleFileUploadChange}
                 />
-            <RecordTable records={validatedRecords} />
+            {fileIsUploaded
+            ? <RecordTable records={validatedRecords} />
+            : <p>No results, please upload a file to process</p>
+            }
         </div>
     );
 }
--- react/app/src/components/RecordTable.js
+++ react/app/src/components/RecordTable.js
@@ -1,29 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function RecordTable(props) {
+    const [filteredRecords, setFilteredRecords] = useState(props.records);
 
-    const recordTableItems = props.records.map(item => {
+    // function RecordTableHeaders() {
+    //     const [firstItem] = props.records;
+    //     console.log('firstItem', firstItem)
+    //     const itemHeaders = Object.keys(firstItem).map(header => {
+    //         if(header !== 'record') {
+    //            return <th>{header}</th>
+    //         }
+    //         return ''
+    //     });
+    //     const recordHeaders = Object.keys(firstItem.record).map(header => <th>{header}</th>)
+    //     const headers = itemHeaders.concat(recordHeaders)
+    //     return (
+    //         <>
+    //         {headers}
+    //         </>
+    //     )
+    // }
+
+    const recordTableItems = filteredRecords.map(item => {
         console.log(item)
+        let validationMessage = '';
+        if(!item.isUniqueReference && !item.isValidTotalAmount) {
+            validationMessage = `Reference ${item.record.reference} is not unique and total amount of ${item.record.totalAmount} is not correct`;
+        }
+        if(!item.isUniqueReference && item.isValidTotalAmount) {
+            validationMessage = `Reference ${item.record.reference} is not unique`;
+        }
+        if(item.isUniqueReference && !item.isValidTotalAmount) {
+            validationMessage = `Total amount of ${item.record.totalAmount} is not correct`;
+        }
+
         return (
             <tr>
-                <td>{item.isUniqueReference.toString()}</td>
-                <td>{item.isValidTotalAmount.toString()}</td>
+                <td>{validationMessage}</td>
                 <td>{item.record.reference}</td>
                 <td>{item.record.name}</td>
                 <td>{item.record.phoneNumber}</td>
                 <td>{item.record.subscription}</td>
-                <td>{item.record.startAmount}</td>
-                <td>{item.record.discount}</td>
-                <td>{item.record.totalAmount}</td>
+                <td>€ {item.record.startAmount}</td>
+                <td>- {Number.isInteger(item.record.discount) ? '€ ' + item.record.discount : item.record.discount * 100 + '%'}</td>
+                <td>€ {item.record.totalAmount}</td>
             </tr>
         )
     })
 
+    function handleFilterRecords(e) {
+        const filter = e.target.value;
+        console.log('filter', filter)
+        if(filter === 'all') {
+            setFilteredRecords(props.records);
+        }
+        if(filter === 'valid') {
+            setFilteredRecords(props.records.filter(item => item.isUniqueReference === true && item.isValidTotalAmount === true))
+        }
+        if(filter === 'invalid') {
+            setFilteredRecords(props.records.filter(item => item.isUniqueReference === false || item.isValidTotalAmount === false))
+        }
+        console.log('filteredRecords', filteredRecords)
+    }
+
     return (
         <>
         <p>Record list</p>
-        <p>Number of records {props.records.length}</p>
+        <p>Number of records {filteredRecords.length}</p>
+        <button onClick={handleFilterRecords} value="all">All records</button>
+        <button onClick={handleFilterRecords} value="valid">Valid records</button>
+        <button onClick={handleFilterRecords} value="invalid">Invalid records</button>
         <table>
+            {/* <RecordTableHeaders /> */}
+            <tr>
+                <th>Validation</th>
+                <th>Reference</th>
+                <th>Name</th>
+                <th>Phone number</th>
+                <th>Subscription</th>
+                <th>Amount</th>
+                <th>Discount</th>
+                <th>Total amount</th>
+            </tr>
             {recordTableItems}
         </table>
         </>
